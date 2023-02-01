e30218cf3383785faf614c0ba2900d960f37a3e7
FelpsSouza <73674525+FelpsSouza@users.noreply.github.com>
2023-02-01 10:59:17 -0300

    Update config.py
    Removendo informações de API_key e API_Secret

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 from binance.client import Client
 import pandas as pd
-API_KEY = "QO9yXYOu0nRfdwXZsXBtJpXv5B6I2JFgdAVGy9x8hjtI4CNCH1PrKskoI5z41OEe"
-API_SECRET = "jYT9dLwKD65lEUejmihyMDJJ8C46G4KBoHaUuX14J5HxkVL3Fxd9ACqTvXLOVhU1"
+API_KEY = ""
+API_SECRET = ""
 
 client = Client(API_KEY, API_SECRET)
 
-sList = ['btcusdt',
+'''sList = ['btcusdt',
          'bnbusdt',
          'ethusdt',
          'dogeusdt',
-         'btcbusd']
+         'btcbusd']'''
 
 symbol = 'btcbusd'
 
@@ -18,7 +18,7 @@ quantity = 0.005
 tframe = '1m'
 
 
-fWsLink = f'wss://fstream3.binance.com/ws/{symbol}@kline_{tframe}'
+fWsLink = f'wss://fstream3.binance.com/ws/{symbol}@kline_{tframe}' #TODO: Enviar para Hklines.py
 
 histOrder = client.futures_get_all_orders(symbol=symbol, limit=1)
 '''for i in histOrder:
